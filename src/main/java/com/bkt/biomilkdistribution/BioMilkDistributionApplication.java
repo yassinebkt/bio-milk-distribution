f75870ad856ea50cec70a9d6b23b889586c02122
yassinebkt <yassine753357@gmail.com>
2019-08-27 23:16:53 +0200

    initial commit of the project

---
+++ src/main/java/com/bkt/biomilkdistribution/BioMilkDistributionApplication.java
@@ -0,0 +1,13 @@
+package com.bkt.biomilkdistribution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BioMilkDistributionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BioMilkDistributionApplication.class, args);
+	}
+
+}
